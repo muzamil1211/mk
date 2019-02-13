a2fc78788462b691994c7cf397f902597e7ad9d2
muzamil1211 <47587932+muzamil1211@users.noreply.github.com>
2019-02-13 12:16:22 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+c = input()
+
+if(c=='A' or c=='a' or c=='E' or c =='e' or c=='I'
+ or ch=='i' or c=='O' or ch=='o' or c=='U' or c=='u'):
+    print( c," Vowel")
+else:
+    print(ch, "is a Consonant")
